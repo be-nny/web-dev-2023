77ccd64e28324b4230f12881aa43309104c7b188
benabbott <ben.abbott1717@gmail.com>
2023-03-06 11:33:58 +0000

    nav bar v1.0

---
+++ styles/navbar.php
@@ -0,0 +1,11 @@
+<?php
+echo '
+<div class="nav_bar">
+    <ul>
+        <li id="home">Home</li>
+        <li id="memory">Play Pairs</li>
+        <li id="leaderboard">Leader Board</li>
+        <li id="register">Register</li>
+    </ul>
+</div>
+';
