04e27462cce365f241aaf8bce34610a62b872219
benabbott <ben.abbott1717@gmail.com>
2023-03-09 11:38:20 +0000

    css and emoji assets

--- pages/index.php
+++ pages/index.php
@@ -3,11 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <title>Title</title>
-
-    <link rel="stylesheet" href="css/">
-
 </head>
 <body>
-    <?php include 'navbar.php';?>
+    <?php include 'navbar.php'; @getNavBar()?>
+    <div id="main">
+
+    </div>
 </body>
 </html>
--- pages/navbar.php
+++ pages/navbar.php
@@ -1,11 +1,31 @@
 <?php
-echo '
-<div class="nav_bar">
-    <ul>
-        <li id="home">Home</li>
-        <li id="memory">Play Pairs</li>
-        <li id="leaderboard">Leader Board</li>
-        <li id="register">Register</li>
-    </ul>
-</div>
-';
+function getNavBar(): void {
+    $uname = $_COOKIE['uname'];
+
+    if(!isset($uname)){
+        echo '
+        <link rel="stylesheet" href="/css/nav_bar_styles.css">
+        
+        <div class="nav_bar">
+            <ul>
+                <li id="home">Home</li>
+                <li id="memory">Play Pairs</li>
+                <li id="register">Register</li>
+            </ul>
+        </div>
+    ';
+
+    } else {
+        echo '
+        <link rel="stylesheet" href="/css/nav_bar_styles.css">
+        
+        <div class="nav_bar">
+            <ul>
+                <li id="home">Home</li>
+                <li id="memory">Play Pairs</li>
+                <li id="leaderboard">Leader Board</li>
+            </ul>
+        </div>
+    ';
+    }
+}
